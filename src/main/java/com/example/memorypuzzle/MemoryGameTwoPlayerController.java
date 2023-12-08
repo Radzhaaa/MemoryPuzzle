0bcf568cbc016f70de88a3996921f98f9bbb6350
Radzhaaa <radzhaaa@bk.ru>
2023-12-08 22:55:29 +0300

    gone game logic for two players

--- src/main/java/com/example/memorypuzzle/MemoryGameTwoPlayerController.java
+++ src/main/java/com/example/memorypuzzle/MemoryGameTwoPlayerController.java
@@ -3,33 +3,40 @@ package com.example.memorypuzzle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ResourceBundle;
 
-public class MemoryGameTwoPlayerController {
+public class MemoryGameTwoPlayerController implements Initializable {
+    private ArrayList<MemoryCard> cardsInGame;
 
     @FXML
     private Button backButton;
 
     @FXML
-    private Label correctLabelOne;
+    private Label correctLabel1;
 
     @FXML
-    private Label correctLabelTwo;
+    private Label correctLabel2;
 
     @FXML
-    private Label guessesLabelOne;
+    private Label guessesLabel1;
 
     @FXML
-    private Label guessesLabelTwo;
+    private Label guessesLabel2;
 
     @FXML
     private ImageView imageView;
@@ -40,6 +47,15 @@ public class MemoryGameTwoPlayerController {
     @FXML
     private Button playButton;
 
+    private MemoryCard card1, card2;
+    private int numOfGuesses1;
+    private int numOfMatches1;
+
+    private int numOfGuesses2;
+    private int numOfMatches2;
+
+    private int whoIs = 1;
+
     @FXML
     void BackView(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Room.fxml"));
@@ -49,9 +65,102 @@ public class MemoryGameTwoPlayerController {
         stage.show();
     }
 
-    @FXML
-    void playAgain(ActionEvent event) {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeImageView();
+        playAgain();
+    }
 
+    private void initializeImageView() {
+        for (int i = 0; i < imagesFlowPane.getChildren().size(); i++) {
+            ImageView imageView = (ImageView) imagesFlowPane.getChildren().get(i);
+            imageView.setImage(new Image(Card.class.getResourceAsStream("images/back_of_card.png")));
+            imageView.setUserData(i);
+
+            imageView.setOnMouseClicked(mouseEvent -> {
+                flipCard((int) imageView.getUserData());
+            });
+        }
+    }
+
+    private void flipAllCards() {
+        for (int i = 0; i < cardsInGame.size(); i++) {
+            ImageView imageView = (ImageView) imagesFlowPane.getChildren().get(i);
+            MemoryCard card = cardsInGame.get(i);
+            if (card.isMatched()) {
+                imageView.setImage(card.getImage());
+            } else {
+                imageView.setImage(card.getBackOfCardImage());
+            }
+        }
+    }
+
+    private void flipCard(int indexOfCard) {
+        if (card1 == null && card2 == null) {
+            flipAllCards();
+        }
+        ImageView imageView = (ImageView) imagesFlowPane.getChildren().get(indexOfCard);
+        if (card1 == null) {
+            card1 = cardsInGame.get(indexOfCard);
+            imageView.setImage(card1.getImage());
+        } else if (card2 == null) {
+            if (whoIs == 1){
+                numOfGuesses1++;
+            }else if (whoIs == 2){
+                numOfGuesses2++;
+            }
+            card2 = cardsInGame.get(indexOfCard);
+            imageView.setImage(card2.getImage());
+            checkForMatch();
+            updateLabels();
+        }
+
+    }
+
+    @FXML
+    void playAgain() {
+        card1 = null;
+        card2 = null;
+        DeckOfCards deck = new DeckOfCards();
+        deck.shuffle();
+        cardsInGame = new ArrayList<>();
+
+        for (int i = 0; i < imagesFlowPane.getChildren().size() / 2; i++) {
+            Card cardDealt = deck.dealTopCard();
+            cardsInGame.add(new MemoryCard(cardDealt.getSuit(), cardDealt.getFaceName()));
+            cardsInGame.add(new MemoryCard(cardDealt.getSuit(), cardDealt.getFaceName()));
+        }
+        Collections.shuffle(cardsInGame);
+        flipAllCards();
+    }
+
+    private void updateLabels() {
+        correctLabel1.setText(Integer.toString(numOfMatches1));
+        guessesLabel1.setText(Integer.toString(numOfGuesses1));
+        correctLabel2.setText(Integer.toString(numOfMatches2));
+        guessesLabel2.setText(Integer.toString(numOfGuesses2));
+
+    }
+
+    private void checkForMatch() {
+        if (card1.isSameCard(card2) & (card1 != card2) ) {
+            if (whoIs == 1){
+                numOfMatches1++;
+            }else if(whoIs == 2){
+                numOfMatches2++;
+            }
+            card1.setMatched(true);
+            card2.setMatched(true);
+        }else {
+            if(whoIs == 1){
+                whoIs = 2;
+            }else if(whoIs == 2){
+                whoIs = 1;
+            }
+
+        }
+        card1 = null;
+        card2 = null;
     }
 
 }
